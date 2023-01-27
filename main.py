d170735562261c085293d9ebf2f7b27700ae4d17
Vladislav Ivanishchev <aloverdoz1135852@gmail.com>
2023-01-27 16:31:21 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from application.salary import test2
+from application.db.people import test
+from datetime import datetime
+from dirty_main import *
+
+
+print(datetime.now())
+
+if __name__ == '__main__':
+    test2()
+    test()
+    new()
